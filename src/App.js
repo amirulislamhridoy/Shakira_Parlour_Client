78a7970dbef0fe29202fb9d26b0ffd3107662fa4
Your Name <hossainhridoy@30687@gmail.com>
2022-07-27 20:56:45 +0600

    header

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import LandingPage from './Pages/LandingPage/LandingPage';
 
 function App() {
+  const i = './images/Ellipse 90.png'
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Routes>
         <Route path='/' element={<LandingPage />} />
       </Routes>
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,11 +1,33 @@
 import React from "react";
+import icon from "../../icons/icons_without_bg.png";
+import {Link} from 'react-router-dom'
 
 const Header = () => {
+  const btn = (
+    <>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/ourPortfolio'>Our Portfolio</Link></li>
+      <li><Link to='/ourTeam'>Our Team</Link></li>
+      <li><Link to='/contactUs'>Contact Us</Link></li>
+      <li><Link to='/login'>Login</Link></li>
+    </>
+  );
   return (
     <nav>
       <div class="navbar bg-base-100">
         <div class="navbar-start">
-          <div class="dropdown">
+          <Link to='' class="btn btn-ghost">
+            <img className="w-10" src={icon} alt="" />
+            <span><b>Shakira</b> <br /> Parlour</span>
+          </Link>
+        </div>
+        <div class="navbar-end hidden lg:flex">
+          <ul class="menu menu-horizontal p-0">
+            {btn}
+          </ul>
+        </div>
+        <div class="navbar-end lg:hidden">
+        <div class="dropdown dropdown-end">
             <label tabindex="0" class="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -24,74 +46,10 @@ const Header = () => {
             </label>
             <ul
               tabindex="0"
-              class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li tabindex="0">
-                <a class="justify-between">
-                  Parent
-                  <svg
-                    class="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul class="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              class="menu menu-compact dropdown-content mt-3 mr-4 p-2 shadow bg-base-100 rounded-box w-36"
+            >{btn}
             </ul>
           </div>
-          <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div class="navbar-center hidden lg:flex">
-          <ul class="menu menu-horizontal p-0">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabindex="0">
-              <a>
-                Parent
-                <svg
-                  class="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul class="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <div class="navbar-end">
-          <a class="btn">Get started</a>
         </div>
       </div>
     </nav>
