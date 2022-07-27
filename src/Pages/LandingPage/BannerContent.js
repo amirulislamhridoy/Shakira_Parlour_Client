6bcde2c9f40a370ddfb79ead01a90b0130c7aa1e
Your Name <hossainhridoy@30687@gmail.com>
2022-07-28 00:01:28 +0600

    LandingPage banner photo responsive

--- src/Pages/LandingPage/BannerContent.js
+++ src/Pages/LandingPage/BannerContent.js
@@ -5,12 +5,12 @@ const BannerContent = () => {
     return (
         <div className='lg:flex items-center lg:pt-8 pt-0 pb-10 lg:px-2 px-5'>
             <div className='flex-1 lg:mb-0 mb-5'>
-                <h1 className='text-5xl font-bold leading-tight'>BEAUTY SALON <br /> FOR EVERY WOMEN</h1>
+                <h1 className='text-4xl lg:text-5xl font-bold leading-tight'>BEAUTY SALON <br /> FOR EVERY WOMEN</h1>
                 <p className='text-base mt-6 mb-8 max-w-[400px]'>Our salon service is very good. Because our employees are very experienced. They can finish work smoothly.</p>
                 <button className='btn btn-primary text-white'>Get an Appointment</button>
             </div>
             <div className=''>
-                <img className='mx-auto max-w-[484px] max-h-[478px] rounded-xl' src={bannerImg} alt="Girl Photo" />
+                <img className='mx-auto lg:max-w-[484px] lg:max-h-[478px] rounded-xl' src={bannerImg} alt="Girl Photo" />
             </div>
         </div>
     );
