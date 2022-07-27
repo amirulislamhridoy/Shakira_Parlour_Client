d367c8df29f4aa6ec0e988018ca6456964d64ecd
Your Name <hossainhridoy@30687@gmail.com>
2022-07-27 23:56:19 +0600

    services responsive

--- src/Pages/LandingPage/Services.js
+++ src/Pages/LandingPage/Services.js
@@ -11,11 +11,11 @@ const Services = () => {
     }, [])
     return (
         <section className='max-w-7xl mx-auto'>
-            <h2 className='text-4xl font-bold text-center mt-20 mb-16'>Our Awesome <span className='text-primary'>Services</span></h2>
-            <div className='flex justify-between'>
+            <h2 className='text-4xl font-bold text-center mt-20 mb-12'>Our Awesome <span className='text-primary'>Services</span></h2>
+            <div className='flex justify-between flex-wrap'>
                 {services.map(service => <SingleService service={service}></SingleService>)}
             </div>
-            <div className='mt-7 mb-10 text-center'>
+            <div className='mt-3 mb-10 text-center'>
                 <button className='btn btn-primary text-white'>Explore more</button>
             </div>
         </section>
--- src/Pages/LandingPage/SingleService.js
+++ src/Pages/LandingPage/SingleService.js
@@ -3,7 +3,7 @@ import React from "react";
 const SingleService = ({service}) => {
     const {img, name, description, taka} = service
   return (
-    <div class="card w-96 bg-base-100 text-center hover:drop-shadow-xl">
+    <div class="card w-96 bg-base-100 text-center hover:drop-shadow-xl mx-auto my-2.5">
       <div class="card-body">
         <img className='w-14 mx-auto' src={img} alt='' />
         <h2 class="text-2xl font-semibold text-center">{name}</h2>
