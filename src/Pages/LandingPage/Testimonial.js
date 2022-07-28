0208475bbb681db47722f8d04732c0db19a1415a
Your Name <hossainhridoy@30687@gmail.com>
2022-07-29 01:14:50 +0600

    testimonial responsive

--- src/Pages/LandingPage/Testimonial.js
+++ src/Pages/LandingPage/Testimonial.js
@@ -6,12 +6,37 @@ import { useEffect } from "react";
 import axios from "axios";
 
 const Testimonial = () => {
-  const settings = {
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 3
+  // };
+  var settings = {
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   const [ratings, setRatings] = useState([]);
   useEffect(() => {
