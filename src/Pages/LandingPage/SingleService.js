2937b4af83595fa70341c8cabc80b117db32a45c
Your Name <hossainhridoy@30687@gmail.com>
2022-08-24 15:39:29 +0600

    add button in every service

--- src/Pages/LandingPage/SingleService.js
+++ src/Pages/LandingPage/SingleService.js
@@ -5,12 +5,13 @@ const SingleService = ({service}) => {
   const navigate = useNavigate()
     const {img, name, description, taka, _id} = service
   return (
-    <div onClick={() => navigate(`/service/${_id}`)} className="card w-96 bg-base-100 text-center hover:drop-shadow-xl mx-auto my-2.5 cursor-pointer">
+    <div className="card w-96 bg-base-100 text-center hover:drop-shadow-xl mx-auto my-2.5 cursor-pointer">
       <div className="card-body">
         <img className='w-14 mx-auto' src={img} alt='' />
         <h2 className="text-2xl font-semibold text-center">{name}</h2>
         <p className='text-primary font-medium'>${taka}</p>
         <p>{description}</p>
+        <button onClick={() => navigate(`/service/${_id}`)} className='btn btn-primary'>You want to Book</button>
       </div>
     </div>
   );
