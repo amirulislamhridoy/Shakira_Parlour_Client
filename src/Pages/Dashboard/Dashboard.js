225cc4465d5c6ad2182b6a939c55effcf16b7469
Your Name <hossainhridoy@30687@gmail.com>
2022-08-24 16:09:57 +0600

    manageServices

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -4,14 +4,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Outlet } from "react-router-dom";
 import DashboardHeader from "./DashboardHeader";
 import PortfolioCustomLink from "./PortfolioCustomLink";
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import useAdmin from "../../hook/useAdmin";
 
 const Dashboard = () => {
   const [user, loading, error] = useAuthState(auth);
-  const [admin] = useAdmin(user)
-  
+  const [admin] = useAdmin(user);
+
   return (
     <section className="max-w-7xl mx-auto">
       <DashboardHeader />
@@ -22,68 +22,91 @@ const Dashboard = () => {
             htmlFor="my-drawer-2"
             className="btn btn-sm mt-2 btn-primary drawer-button lg:hidden"
           >
-            Open Sidebar
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+              />
+            </svg>
           </label>
           <Outlet />
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
           <ul className="menu p-4 overflow-y-auto w-60 bg-base-100 text-base-content">
-            {admin ? <>
-              <li>
-              <PortfolioCustomLink to="/dashboard">
-                <FontAwesomeIcon className="h-5 w-5" icon={faBasketShopping} />
-                Order List
-              </PortfolioCustomLink>
-            </li>
-            <li>
-              <PortfolioCustomLink to="/dashboard/addService">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4v16m8-8H4"
-                  />
-                </svg>
-                Add service
-              </PortfolioCustomLink>
-            </li>
-            <li>
-              <PortfolioCustomLink to="/dashboard/makeAdmin">
-                <i className="fa-solid fa-user-plus"></i>
-                Make Admin
-              </PortfolioCustomLink>
-            </li>
-            <li>
-              <PortfolioCustomLink to="/dashboard/manageServices">
-              <i className="fa-solid fa-bars-progress"></i>Manage Services
-              </PortfolioCustomLink>
-            </li>
-            </>
-            : <>
-              <li>
-              <PortfolioCustomLink to="/dashboard">
-              <i className="fa-solid fa-cart-shopping"></i>Book
-              </PortfolioCustomLink>
-            </li>
-            <li>
-              <PortfolioCustomLink to="/dashboard/bookingList">
-              <FontAwesomeIcon className="h-5 w-5" icon={faBasketShopping} />Booking List
-              </PortfolioCustomLink>
-            </li>
-            <li>
-              <PortfolioCustomLink to="/dashboard/review">
-              <i className="fa-solid fa-message"></i>Review
-              </PortfolioCustomLink>
-            </li>
-            </>} 
+            {admin ? (
+              <>
+                <li>
+                  <PortfolioCustomLink to="/dashboard">
+                    <FontAwesomeIcon
+                      className="h-5 w-5"
+                      icon={faBasketShopping}
+                    />
+                    Order List
+                  </PortfolioCustomLink>
+                </li>
+                <li>
+                  <PortfolioCustomLink to="/dashboard/addService">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 4v16m8-8H4"
+                      />
+                    </svg>
+                    Add service
+                  </PortfolioCustomLink>
+                </li>
+                <li>
+                  <PortfolioCustomLink to="/dashboard/makeAdmin">
+                    <i className="fa-solid fa-user-plus"></i>
+                    Make Admin
+                  </PortfolioCustomLink>
+                </li>
+                <li>
+                  <PortfolioCustomLink to="/dashboard/manageServices">
+                    <i className="fa-solid fa-bars-progress"></i>Manage Services
+                  </PortfolioCustomLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <PortfolioCustomLink to="/dashboard">
+                    <i className="fa-solid fa-cart-shopping"></i>Book
+                  </PortfolioCustomLink>
+                </li>
+                <li>
+                  <PortfolioCustomLink to="/dashboard/bookingList">
+                    <FontAwesomeIcon
+                      className="h-5 w-5"
+                      icon={faBasketShopping}
+                    />
+                    Booking List
+                  </PortfolioCustomLink>
+                </li>
+                <li>
+                  <PortfolioCustomLink to="/dashboard/review">
+                    <i className="fa-solid fa-message"></i>Review
+                  </PortfolioCustomLink>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
--- src/Pages/Dashboard/ManageServices.js
+++ src/Pages/Dashboard/ManageServices.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../../Shared/Loading/Loading";
 
 const ManageServices = () => {
-    return (
-        <div>
-            manage services
-        </div>
-    );
+  const { isLoading, error, data } = useQuery(["repoData"], () =>
+    fetch("http://localhost:5000/manageServices").then((res) => res.json())
+  );
+  if(isLoading){
+    return <Loading />
+  }
+
+  return (
+    <section>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Service Name</th>
+              <th>Taka</th>
+              <th>Photo</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((service, index) => (
+              <tr key={service._id}>
+                <th>{index + 1}</th>
+                <td>{service.name}</td>
+                <td>{service.taka}</td>
+                <td className='p-0'>
+                  <div className="avatar">
+                    <div className="w-24 mask mask-hexagon">
+                      <img src={service.img} alt='' />
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
 };
 
 export default ManageServices;
